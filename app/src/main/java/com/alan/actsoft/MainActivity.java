75314259711fe8b9f7cfcc76cbb75d9be265fb58
lokesh <>
2020-05-29 00:27:06 +0530

    Integrated the Alan SDK with demo application.

--- app/src/main/java/com/alan/actsoft/MainActivity.java
+++ app/src/main/java/com/alan/actsoft/MainActivity.java
@@ -2,13 +2,33 @@ package com.alan.actsoft;
 
 import android.os.Bundle;
 
+import com.alan.actsoft.alan.Alan;
+import com.alan.alansdk.button.AlanButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private AlanButton alanButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configAlanVoice();
+    }
+
+
+
+    /**
+     * Method configures the alan voice command UI with application, initializes the Alan command
+     * and voice command listener with Alan SDK.
+     */
+    private void configAlanVoice(){
+        if(this.alanButton == null) {
+            alanButton = findViewById(R.id.alan_button);
+            Alan.getInstance().setAlanButton(alanButton);
+            alanButton.setButtonAlign(AlanButton.BUTTON_RIGHT);
+        }
     }
 
     @Override
