491ccbc7b03b90726dda9f92b159d2cb6eda02e5
Alan Android <55826308+lokeshsm77@users.noreply.github.com>
2020-05-28 23:22:03 +0530

    Add files via upload

---
+++ app/src/main/java/com/alan/actsoft/FormActivity.java
@@ -0,0 +1,38 @@
+package com.alan.actsoft;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.covid_test_form);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
